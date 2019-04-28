e3f34c6a57bf557ad40bfa245f29ab2645a48595
Nourhan <eng.nhamda@yahoo.com>
2019-04-28 12:23:17 +0200

    added report grid title

--- app/code/local/StylishEve/OrderActionsButtons/Block/Adminhtml/Reportsarchive.php
+++ app/code/local/StylishEve/OrderActionsButtons/Block/Adminhtml/Reportsarchive.php
@@ -7,11 +7,22 @@ class StylishEve_OrderActionsButtons_Block_Adminhtml_Reportsarchive extends Mage
     public function __construct()
     {
 
-        // $this->_controller = "adminhtml_orderbutton";
-        // $this->_blockGroup = "orderactionsbuttons";
-        // $this->_headerText = Mage::helper("orderactionsbuttons")->__("Reportsarchive Manager");
-        parent::__construct();
-        // $this->_removeButton('add');
+         $this->_controller = "adminhtml_orderbutton";
+         $this->_blockGroup = "orderactionsbuttons";
+         $this->_headerText = Mage::helper("orderactionsbuttons")->__("Reportsarchive Manager");
+         /**
+          * Note:
+          *  replace
+          *         parent::__construct();
+          *  with
+          *         $this->setTemplate('widget/grid/container.phtml');
+          *         $this->getGridHtml();
+          *
+          *  - for add report grid title and, use report model
+          */
+         $this->setTemplate('widget/grid/container.phtml');
+         $this->getGridHtml();
+         $this->_removeButton('add');
 
     }
 
